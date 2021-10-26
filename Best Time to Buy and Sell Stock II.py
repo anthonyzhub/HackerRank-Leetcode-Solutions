1e1727f1bde102c09b6f878ba0c7219b7e5ebc2e
Anthony <zamora.anthony1234@gmail.com>
2021-10-26 17:10:13 -0400

    Update Best Time to Buy and Sell Stock II.py

--- Best Time to Buy and Sell Stock II.py
+++ Best Time to Buy and Sell Stock II.py
@@ -7,9 +7,11 @@ class Solution:
         if len(prices) == 1:
             return 0
         
+        # Iterate list
         totalProfit = 0
-        for idx in range(1, len(prices)):
+        for idx in range(1, len(prices)): # NOTE <= Starting at 2 because loop will check for previous element
             
+            # If current price is greater than previous price, add profit to totalProfit
             if prices[idx] > prices[idx - 1]:
                 totalProfit += prices[idx] - prices[idx - 1]
                 
