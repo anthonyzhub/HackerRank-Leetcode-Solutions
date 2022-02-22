6c9d659f8429325ad1ec4fccd6eb62253092ff3d
Anthony <zamora.anthony1234@gmail.com>
2022-02-22 16:11:56 -0500

    Update Validate Binary Search Tree.py

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/validate-binary-search-tree/submissions/
+# https://leetcode.com/problems/validate-binary-search-tree/
 
 # Definition for a binary tree node.
 # class TreeNode:
