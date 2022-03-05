32324dfbdc8ece546022b2ce66b8a6f94d456ff7
Anthony <zamora.anthony1234@gmail.com>
2022-03-05 11:53:46 -0500

    Create Count Primes.java

---
+++ Count Primes.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/count-primes/
+
+class Solution {
+    
+    public int countPrimes(int n) {
+        
+        // IMPORTANT: A prime number must be greater than 1 and is only divisible by 1 and itself.
+        
+        // Create a boolean array
+        boolean[] isNumPrime = new boolean[n];
+        Arrays.fill(isNumPrime, true);
+        
+        // Traverse to halfway point of n
+        // NOTE: First products of sqrt(n) is the same as the products from sqrt(n) to n
+        for (int i=2; i*i<n; i++) {
+            
+            // Check if element at ith position is a prime number
+            if (isNumPrime[i]) {
+                
+                // Define every multiple of i into a non-prime number
+                for (int j=i*i; j<n; j+=i) {
+                    isNumPrime[j] = false;
+                }
+            }
+        }
+        
+        // Count how many prime numbers there are left
+        int counter = 0;
+        for (int i=2; i<n; i++) {
+            if (isNumPrime[i]) {counter++;}
+        }
+        
+        return counter;
+    }
+}
