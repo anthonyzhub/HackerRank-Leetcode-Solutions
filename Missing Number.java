5f2bff07c77ce0be80e5aa4a6e63eee1922a6bc5
Anthony <zamora.anthony1234@gmail.com>
2022-04-20 10:09:49 -0400

    Update Missing Number.java

--- Missing Number.java
+++ Missing Number.java
@@ -17,7 +17,7 @@ class Solution {
         }
         
         // If function is still continuing, then missing digit is the last.
-        // E.g., [0, 1] array has a length of 2. The missing digit is then 2.
+        // E.g., [0, 1] has a length of 2, but 2 is not inside the list. Therefore, it is the missing digit.
         return nums.length;
     }
 }
