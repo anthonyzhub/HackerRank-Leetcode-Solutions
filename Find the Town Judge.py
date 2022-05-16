37b14cff0c338cb429f5fd9e0ff03898267a76d5
Anthony <zamora.anthony1234@gmail.com>
2022-05-16 10:51:29 -0400

    Update Find the Town Judge.py

--- Find the Town Judge.py
+++ Find the Town Judge.py
@@ -3,32 +3,31 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
-        # If list is empty, return -1
-        if len(trust) == 0:
-            return -1
+        # Create 2 new list - trustCount and trustAnother
+        # NOTE: "n + 1" was added because "n" starts at 1
+        trustCount = [0] * (n + 1)
+        trustAnother = [False] * (n + 1)
         
-        # If list only has one element, return 2nd element inside of only pair
-        elif len(trust) == 1:
-            return trust[0][1]
-        
-        # Create a list that holds a person and the people they trust
-        ans = list()
-        
-        # Traverse list and add each person to ans
+        # Traverse trust list
         for pair in trust:
-            ans.append(pair[0])
-        
-        # Traverse ans list
-        judge = 0
-        ans.sort()
-        for idx in range(len(ans)):
             
-            # If current person isn't the same as the one before, then update judge
-            if ans[idx] - 1 != ans[idx - 1] and idx == 0:
-                judge = idx
-                
-            # If everyone trust each other, then there's no judge so return -1
-            elif ans[idx] - 1 != ans[idx - 1] and idx:
-                return -1
+            # Breakdown pair
+            truster = pair[0]
+            trustee = pair[1]
             
-        return judge
+            # Add variable to appropriate list
+            # NOTE: Since there's no 0th person, person 1 will be on the 1st index
+            trustAnother[truster] = True
+            trustCount[trustee] += 1
+            
+        # Traverse trustCount and trustAnother
+        # NOTE: Both lists share the same size and loop starts at 1 because there's no 0th person.
+        for i in range(1, len(trustCount)):
+            
+            # If ith person doesn't trust anyone and everyone, except for himself, trust them, then they are the judge.
+            # NOTE: n - 1 where n = population people need to trust him. The judge doesn't even trust himself.
+            if trustAnother[i] == False and trustCount[i] == n - 1:
+                return i
+            
+        # If no judge was found, return -1
+        return -1
