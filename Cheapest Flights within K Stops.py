21c10223bf03a9a245d3c32f2de7161b2c9dd308
Anthony <zamora.anthony1234@gmail.com>
2022-07-23 14:22:38 -0400

    Create Cheapest Flights within K Stops.py

---
+++ Cheapest Flights within K Stops.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+import heapq
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        # Create a dictionary of adjacent nodes
+        adj = {airport: list() for airport in range(n)}
+        
+        # Populate dictionary with flight information
+        for curSrc, curDest, curCost in flights:
+            adj[curSrc].append((curDest, curCost))
+            
+        # Create a heap
+        minHeap = [(0, src, 0)]
+        heapq.heapify(minHeap)
+        
+        # Create a list of visited nodes
+        visited = [k + 1] * n
+        
+        # Iterate heap
+        while minHeap:
+            
+            # Get cost, node, depth level from popped element
+            cost, node, level = heappop(minHeap)
+            
+            # If node equals to dest, return cost
+            if node == dst:
+                return cost
+            
+            # If level is less than stops left or level is greater or equal to nth index in visited, skip to next loop
+            if level > k or level >= visited[node]:
+                continue
+                
+            # Update element at nth index in visited
+            visited[node] = level
+            
+            # Add connecting flights from node to heap
+            for curDest, curCost in adj[node]:
+                heappush(minHeap, (cost + curCost, curDest, level + 1))
+                
+        return -1
