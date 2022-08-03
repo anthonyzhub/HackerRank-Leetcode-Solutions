adc59c01ca6c18095ac67624ff0e34dd4a088d6c
Anthony <zamora.anthony1234@gmail.com>
2022-08-03 19:20:23 -0400

    Create Bulb Switcher.java

---
+++ Bulb Switcher.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/bulb-switcher/
+
+class Solution {
+    public int bulbSwitch(int n) {
+        
+        // Square root of n bulbs will be turned on
+        return (int) Math.sqrt(n);
+    }
+}
