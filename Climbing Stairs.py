20af2152d3fccd8375b4a2b5290fe8b2cd98a015
Anthony <zamora.anthony1234@gmail.com>
2022-11-17 20:14:48 -0500

    Update Climbing Stairs.py

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -1,17 +1,25 @@
+# https://leetcode.com/problems/climbing-stairs/
+
 class Solution:
     
     def climbStairs(self, n: int) -> int:
         
-        # Create a list with a few added elements
-        # E.g. 0 and 1 step = 1 unique path
-        #       2 steps = 2 unique paths
-        stepsNeeded = [1, 1]
+        """
+        OBJECTIVE: You are climbing a staircase. It takes n steps to reach the top. Each time you can either
+                    climb 1 or 2 steps. In how many distinct ways can you climb to the top?
         
-        # Fill in list up to n
-        for i in range(2, n+1): # <= "n+1" was added to make "n" inclusive
-            
-            # NOTE: The append function was used, instead of stepsNeeded[i], is because the latter caused an "index out of range" error.
-            # This makes sense because position "i" never existed. If you want to have an "ith" position, then you need to APPEND the element to the list.
-            stepsNeeded.append(stepsNeeded[i-1] + stepsNeeded[i-2])
-            
-        return stepsNeeded[n]
+        Time Complexity: O(n) where n = the input n. The for-loop is iterating from n - 2 times. The " - 2" is
+                        dropped because it is a constant.
+        
+        Space Complexity: O(n) where n = the size of stairs' list. The list will hold n elements.
+        """
+        
+        # Create a list
+        stairs = [1, 2]
+        
+        # Iterate range
+        # NOTE: I started at 2 since the first 2 steps were already calculated
+        for step in range(2, n):
+            stairs.append(stairs[step - 2] + stairs[step - 1])
+        
+        return stairs[n - 1]
