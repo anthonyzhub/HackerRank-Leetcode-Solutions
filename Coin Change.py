bf43e050a17c4366b2f4cc2ec07c2d41ccd08659
Anthony <zamora.anthony1234@gmail.com>
2022-11-22 21:03:33 -0500

    Update Coin Change.py

--- Coin Change.py
+++ Coin Change.py
@@ -1,38 +1,41 @@
 # https://leetcode.com/problems/coin-change/
 
+import sys
+
 class Solution:
-    
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        # If amount is 0 and coins list isn't empty, return 0
-        if amount == 0 and len(coins) > 0:
-            return 0
+        """
+        OBJECTIVE: Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
         
-        # If amount is already in coins, return 1
-        if amount in coins:
-            return 1
+        Time Complexity: O((amount + 1) * coins). The outer-loop iterates amount + 1 times and each iteration involves iterating the coins list
         
-        # Create a list for dynamic programming
-        dp = [amount + 1] * (amount + 1)
+        Space Complexity: O(n) where n = length of dp. The dp list's size will always be equal to amount + 1 because that's how the list was initialized
+        """
         
-        # Set 1st element as 0.
-        # NOTE: You need 0 coins to reach 0 amount
+        # Create a dp list where it'll hold N elements and each element has a maximum value of max int
+        dp = [sys.maxsize] * (amount + 1)
+        
+        # Set the 0th element to 0. This is saying, "to reach 0 amount (the index), we only need 0 coins (the value at index)"
         dp[0] = 0
         
-        # Find out how many coins are needed to reach i-value
-        for i in range(1, amount + 1):
+        # Iterate amount range
+        # NOTE: Added +1 because range() is non-inclusive [)
+        for curAmount in range(1, amount + 1):
             
             # Iterate coins list
             for coin in coins:
                 
-                # If coin is greater than or equal to i-value, update the ith index in dp
-                if i - coin >= 0:
+                # If remaining change greater than or equal to 0, save how many coins were needed to reach this amount
+                if curAmount - coin >= 0:
                     
-                    # Keep the minimum number of coins needed to reach i-value
-                    dp[i] = min(dp[i], dp[i - coin] + 1)
-        
-        # If element doesn't equal to its original maximum value, return its new minimum value
-        if dp[amount] != amount + 1:
+                    # Which option has less coins? The one we already have saved or that with 1 additional coin
+                    # NOTE: Remember that the initial value is maximum int
+                    dp[curAmount] = min(dp[curAmount], 1 + dp[curAmount])
+                    
+        # If index at amount isn't still at initial value, return value at index
+        if dp[amount] != sys.maxsize:
             return dp[amount]
         
+        # If function is still continuing, then the amount cannot be reached with any combination of coins
         return -1
