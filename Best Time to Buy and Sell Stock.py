6148a254067c87ee6d22c4dcddca50407c566e64
Anthony <zamora.anthony1234@gmail.com>
2022-12-05 19:11:04 -0500

    Update Best Time to Buy and Sell Stock.py

--- Best Time to Buy and Sell Stock.py
+++ Best Time to Buy and Sell Stock.py
@@ -1,25 +1,37 @@
-# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
-
-import sys
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        # If list only has 1 element, return 0
-        if len(prices) == 1:
-            return 0
+        """
+        OBJECTIVE: You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+        
+        Time Complexity: O(n) where n = length of prices. A while-loop was created to iterate the loop once
+        
+        Space Complexity: O(1) because no additional space was used
+        """
+        
+        # Get length of list
+        n = len(prices)
+        
+        # Create pointers
+        leftPtr = 0
+        rightPtr = 1
+        
+        # Create variable to hold max profit
+        maxProfit = 0
         
         # Iterate list
-        maxProfit = 0
-        minPrice = sys.maxsize # <= Get highest possible number
-        for idx in range(len(prices)):
+        while rightPtr < n:
             
-            # Update variable for smallest price
-            if prices[idx] < minPrice:
-                minPrice = prices[idx]
+            # If selling price is higher than buying, then calculate max profit and update rightPtr
+            if prices[leftPtr] < prices[rightPtr]:
+                maxProfit = max(maxProfit, prices[rightPtr] - prices[leftPtr])
+                rightPtr += 1
             
-            # Check if current profit margin can beat max profit
-            elif prices[idx] - minPrice > maxProfit:
-                maxProfit = prices[idx] - minPrice
-            
+            # If selling price isn't higher than buying, then move leftPtr to rightPtr and update rightPtr
+            else:
+                leftPtr = rightPtr
+                rightPtr += 1
+                
         return maxProfit
