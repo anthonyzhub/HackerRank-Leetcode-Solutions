984b0a969e45dd3aac59beff79ebb335280785e4
Anthony <zamora.anthony1234@gmail.com>
2022-12-09 19:35:27 -0500

    Update Product of Array Except Self.java

--- Product of Array Except Self.java
+++ Product of Array Except Self.java
@@ -1,32 +1,37 @@
-// Link: https://leetcode.com/problems/product-of-array-except-self/
+// https://leetcode.com/problems/product-of-array-except-self/description/
 
 class Solution {
-    
     public int[] productExceptSelf(int[] nums) {
+
+        /*
+        OBJECTIVE: Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
         
-        // Create a new array filled with 1's.
-        int[] answer = new int[nums.length];
-        Arrays.fill(answer, 1);
+        Time Complexity: O(n) where n = length of nums. 2 for-loops were created to iterate the nums. Each loop's runtime is O(n)
         
-        // Create pointers
-        int leftIdx = 1;
-        int rightIdx = 1;
+        Space Complexity: O(n) where n = length of res. A return array had to be created and its size is based off nums' size
+        */
         
-        // Iterate nums
-        for (int i=0; i<nums.length; i++)
-        {
-            // Start at 2nd to last element
-            int answerIdx = nums.length - i - 1;
-            
-            // Update left and right element in answer
-            answer[i] = leftIdx * answer[i];
-            answer[answerIdx] = rightIdx * answer[answerIdx];
-            
-            // Update pointers
-            leftIdx *= nums[i];
-            rightIdx *= nums[answerIdx];
+        // Get length of nums
+        int n = nums.length;
+
+        // Create a return array filled with 1's
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
+
+        // Calculate prefix and update res
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
         }
-        
-        return answer;
+
+        // Calculate postfix and update res
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return res;
     }
 }
