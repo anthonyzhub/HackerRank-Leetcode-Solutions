59b4afc1cd7f62fc001dc0260c2331520b1fb6b4
Anthony <zamora.anthony1234@gmail.com>
2024-01-06 20:40:28 -0500

    Updated solution for Contains Duplicate problem

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -4,24 +4,27 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
 
         /*
-        OBJECTIVE: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
-        Time Complexity: O(n) where n = length of nums. A for-loop was created to transfer all elements from nums to a set.
-
-        Space Complexity: O(n) where n = length of nums. A set was created because it doesn't accept duplicate entries. 
-                          Worst case scenario is that nums doesn't have any duplicate entries
-        */
+         * Time Complexity: O(n) where n = length of nums
+         * Space Complexity: O(n) where n = length of hash set
+         */
         
-        // Convert array to set
-        HashSet<Integer> numsSet = new HashSet<>();
+        // If array only has 1 element, return false
+        if (nums.length == 1) {return false;}
+
+        // Create a hash set and iterate array
+        HashSet<Integer> numberSet = new HashSet<Integer>();
+
         for (int i: nums) {
-            numsSet.add(i);
+
+            // If the number exist in the set, return true
+            if (numberSet.contains(Integer.valueOf(i))) {
+                return true;
+            }
+
+            // If number doesn't exist in set, add it
+            numberSet.add(Integer.valueOf(i));
         }
 
-        // If set and array don't share the same size, then array has a duplicate entry
-        if (nums.length != numsSet.size()) {
-            return true;
-        }
         return false;
     }
 }
