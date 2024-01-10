750300bbd51178c4147c4ec83fd7da884be4fb5b
Anthony <zamora.anthony1234@gmail.com>
2024-01-09 21:50:21 -0500

    Added new solution to Coin Change problem

--- Coin Change.java
+++ Coin Change.java
@@ -1,7 +1,65 @@
 // https://leetcode.com/problems/coin-change/
 
 class Solution {
+
+    public int coinChangeSub(int[] coins, int amount, int[] memo) {
+
+        // If amount is less than 0, then exit because we used too many coins
+        if (amount < 0) {return -1;}
+
+        // If amount is 0, congratulations because we don't need anymore coins
+        if (amount == 0) {return 0;}
+
+        // If an optimal solution has already been calculated for "amount", then return it rather than recompute it.
+        if (memo[amount - 1] != 0) {
+            return memo[amount - 1];
+        }
+
+        // Iterate list of available coins for use
+        int optimalSolution = Integer.MAX_VALUE;
+        for (int coin: coins) {
+
+            // Calculate the least amount of coins needed to reach "amount"
+            int best_solution_for_coin = coinChangeSub(coins, amount - coin, memo);
+
+            // If the least amount of coins is greater than 0 & is less than the previous' optimal solution, then update variable to our new optimal solution
+            if (0 <= best_solution_for_coin && best_solution_for_coin < optimalSolution) {
+                optimalSolution = best_solution_for_coin + 1;
+            }
+        }
+
+        // If optimal solution is -1, then it's impossible to reach "amount" with our "coins"
+        if (optimalSolution == Integer.MAX_VALUE) {
+            memo[amount - 1] = -1;
+        }
+        else {
+            memo[amount - 1] = optimalSolution;
+        }
+
+        return memo[amount - 1];
+    }
+
     public int coinChange(int[] coins, int amount) {
+
+        /*
+         * Time Complexity: O(m * n) => O(m) where m = 1 to amount and n = # of coins. Because of recursion and memoization,
+         * the runtime depends on m. There's a recursive call for coin in coins while amount doesn't equal or become less than 0. 
+         * Memoization also helps to end a recursive call early.
+         * 
+         * Space Complexity: O(m * n) where m = 1 to amount and n = # of coins. Same reason as above. 
+         * 
+         * Reference: https://betterprogramming.pub/learn-dynamic-programming-the-coin-change-problem-22a104478f50 
+         */
+
+        if (amount < 0) {
+            return -1;
+        }
+        else {
+            return coinChangeSub(coins, amount, new int[amount + 1]);
+        }
+    }
+
+    public int coinChange_solutionA(int[] coins, int amount) {
         
         // If amount is 0 and coins list isn't empty, return 0
         if (amount == 0 && coins.length > 0) {return 0;}
