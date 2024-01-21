d30927d9c5de047a2622f3921c5dbc15b110a77a
Anthony <zamora.anthony1234@gmail.com>
2024-01-20 19:23:54 -0500

    Added Java solution for 'Merge 2 Sorted Linked Lists.java'

---
+++ Merge 2 Sorted Lists.java
@@ -0,0 +1,75 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null || list2 == null) {
+            if (list1 == null & list2 == null) {return null;}
+            else if (list1 == null) {return list2;}
+            else if (list2 == null) {return list1;}
+        }
+
+        // If both are the only nodes in the linked list, return them in sorted order
+        if (list1.next == null && list2.next == null) {
+            if (list1.val <= list2.val) {
+                list1.next = list2;
+                return list1;
+            }
+            else {
+                list2.next = list1;
+                return list2;
+            }
+        }
+
+        // Create new nodes
+        ListNode newHead = new ListNode();
+        ListNode curNode = newHead;
+        newHead.next = curNode;
+
+        // Iterate both linked lists until one of them ends first
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                curNode.next = list1;
+                curNode = curNode.next;
+
+                list1 = list1.next;
+            }
+            else {
+                curNode.next = list2;
+                curNode = curNode.next;
+
+                list2 = list2.next;
+            }
+        }
+
+        // Add remaining nodes from list1 to new linked list
+        while (list1 != null) {
+            curNode.next = list1;
+            curNode = curNode.next;
+
+            list1 = list1.next;
+        }
+
+        // Add remaining nodes from list2 to new linked list
+        while (list2 != null) {
+            curNode.next = list2;
+            curNode = curNode.next;
+
+            list2 = list2.next;
+        }
+        
+        return newHead.next;
+    }
+}
