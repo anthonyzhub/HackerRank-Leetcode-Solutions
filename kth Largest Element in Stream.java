e52b91d9389c090560f7810e84988b8087a018d0
Anthony <zamora.anthony1234@gmail.com>
2024-04-13 18:57:55 -0400

    Added 'kth largest element in stream'

--- kth Largest Element in Stream.java
+++ kth Largest Element in Stream.java
@@ -6,6 +6,15 @@ class KthLargest {
     private int k;
 
     public KthLargest(int k, int[] nums) {
+
+        /*
+         * Time Complexity: O(n * logn) where n = number of elements inside the array. In the initializer,
+         *  all the array elements are being pushed to the tree. If heap size exceeds k, then the elements
+         *  are popped from the heap, which also takes O(log n) times.
+         * 
+         * Space Complexity: O(n) where n = number of nodes inside the heap.
+         */
+
         this.k = k;
         this.minHeap = new PriorityQueue<>();
 
@@ -15,13 +24,27 @@ class KthLargest {
         }
 
         // Only keep k elements inside heap
+        // NOTE: You don't need any excessive elements because they won't be used
         while (minHeap.size() > k) {
             minHeap.poll();
         }
     }
     
     public int add(int val) {
+
+        /*
+         * Time Complexity: O(log n) where n = number of nodes inside heap. A heap's insertion property
+         *  takes O(log n) on average. That is with all heaps.
+         * 
+         * Space Complexity: O(1) because you're only creating one additional node and appending it to
+         *  the tree.
+         */
+
+        // Add element to heap
         minHeap.add(Integer.valueOf(val));
+
+        // If heap size exceeds k elements, pop head
+        // NOTE: There is a scenario where initializer will pass an array that has a size smaller than k
         if (minHeap.size() > k) {
             minHeap.poll();
         }
