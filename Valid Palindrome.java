6aef0538da73308bf38db6ab372a11bf8f32e92e
Anthony <zamora.anthony1234@gmail.com>
2024-12-26 15:10:42 -0500

    Update Valid Palindrome.java

--- Valid Palindrome.java
+++ Valid Palindrome.java
@@ -1,45 +1,39 @@
 // https://leetcode.com/problems/valid-palindrome/
 
+/*
+Time Complexity: O(n) where n = length of input string.
+Space Complexity: O(1) because no additional dynamic data structure was created.
+*/
+
 class Solution {
-    public boolean isPalindrome(String s) {
 
-        /*
-         * Time Complexity: O(n) where n = length of string. While-loop is iterating 
-         *  string until the halfway point is reached. Keep in mind, in time and space complexity
-         *  coefficients are dropped. So, O(n) equals to O(n/2).
-         * 
-         * Space Complexity: O(1) because no additional, dynamic data structure
-         *  was created.
-         */
-        
-        // Remove all non-alphanumeric characters and them uncapitalize all letters
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    private String removeNonAlphaNumericCharacters(String inputStr) {
+        return inputStr.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    }
 
-        // If s has 1 or less characters, then it's automatically a palindrone
-        if (s.length() <= 1) {
-            return true;
-        }
-
-        // Create a left and right pointer
+    private boolean traverseStringBothWays(String inputStr) {
         int leftPtr = 0;
-        int rightPtr = s.length() - 1;
+        int rightPtr = inputStr.length() - 1;
 
-        // Iterate string
-        while (leftPtr <= rightPtr) {
+        while (leftPtr < rightPtr) {
 
-            // If both characters don't equal each other, return false.
-            // This string isn't a palindrone
-            if (s.charAt(leftPtr) != s.charAt(rightPtr)) {
+            if (inputStr.charAt(leftPtr) != inputStr.charAt(rightPtr)) {
                 return false;
             }
 
-            // Move both pointers closer to the middle of the string
-            leftPtr += 1;
-            rightPtr -= 1;
+            leftPtr++;
+            rightPtr--;
         }
 
-        // If function is still continuing, return true.
-        // This string is a palindrone.
         return true;
+    }
+
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() <= 1) {
+            return true;
+        }
+
+        String strippedStr = removeNonAlphaNumericCharacters(s);
+        return traverseStringBothWays(strippedStr);
     }
 }
