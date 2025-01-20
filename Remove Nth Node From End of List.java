b6840abf1237c7d7da3ab88671780845c36eef45
Anthony <zamora.anthony1234@gmail.com>
2025-01-20 16:45:05 -0500

    Update Remove Nth Node From End of List.java

--- Remove Nth Node From End of List.java
+++ Remove Nth Node From End of List.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+// https://youtu.be/XVuQxVej6y8?si=wLbsrSn2F0GLpjNd
 
 /**
  * Definition for singly-linked list.
@@ -11,44 +12,38 @@
  * }
  */
 
- class Solution {
+/*
+Time Complexity: O(n) where n = size of linked list. It's important to remember that the question is asking for the nth last element.
+
+Space Complexity: O(1) because no additional dynamic memory was created
+*/
+
+class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
-        /*
-         * Time Complexity: O(n) where n = length of linked list. We have to iterate the entire
-         *  list to find the nth to last node.
-         * 
-         * Space Complexity: O(1) because no dynamic data structure was created. Only 3 nodes were
-         *  created and they take constant space to make.
-         */
-
-        // If head is empty or by itself, return null
         if (head == null || head.next == null) {
             return null;
         }
-        
-        // Create a dummy node to help return value
+
+        // Move rightNode n nodes ahead
+        ListNode rightNode = head;
+        while (n > 0 && rightNode != null) {
+            rightNode = rightNode.next;
+            n--;
+        }
+
+        // Create a dummy node connected to head. This will be used for the return value.
         ListNode dummy = new ListNode();
         dummy.next = head;
 
-        // Create a left and right node pointer
+        // Iterate linked list. Keep in mind, leftNode is n spaces away from rightNode
         ListNode leftNode = dummy;
-        ListNode rightNode = head;
-
-        // Move rightNode n steps
-        for (int i = 0; i < n; i++) {
-            rightNode = rightNode.next;
-        }
-
-        // Iterate linked list until rightNode reaches end of list
         while (rightNode != null) {
             leftNode = leftNode.next;
             rightNode = rightNode.next;
         }
 
-        // Cut target node from linked list
+        // IMPORTANT: At this point, rightNode is null so I have to use leftNode.next.next
         leftNode.next = leftNode.next.next;
-
         return dummy.next;
     }
 }
