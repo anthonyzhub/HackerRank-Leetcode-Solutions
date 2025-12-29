c13a1b2f15fc3e8988edadd0b2a7f420a0d654ca
Anthony <zamora.anthony1234@gmail.com>
2025-12-29 11:58:37 -0500

    Update Sliding Window Maximum.java

--- Sliding Window Maximum.java
+++ Sliding Window Maximum.java
@@ -1,42 +1,44 @@
 // https://leetcode.com/problems/sliding-window-maximum/
 // https://youtu.be/DfljaUwZsOk?si=B8gqsudQB5pC9kdA
-
 /*
-Time Complexity: O(K * (K - N)) where K = input value and N = length of nums. The program iterates the entire input array until the K-sized sliding window goes out of bounds.
+Time Complexity: O(n) where n = length of nums.
 
-Space Complexity: O(K) where K = length of result array. There is a possibility of the size of result array and idxQueue are the same. 
+Space Complexity: O(n) where n = length of deque
 */
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         List<Integer> result = new ArrayList<>();
 
-        Deque<Integer> idxQueue = new ArrayDeque<>();
-        int leftPtr = 0;
-        int rightPtr = 0;
+        // Create a queue
+        // NOTE: This queue will act as a monotonic decreasing queue (e.g., 10, 9, etc.)
+        Deque<Integer> indexDeque = new ArrayDeque<>();
+        int leftPtr = 0, rightPtr = 0;
 
         while (rightPtr < nums.length) {
 
-            // Pop smaller elements from queue
-            while (!idxQueue.isEmpty() && nums[idxQueue.getLast()] < nums[rightPtr]) {
-                idxQueue.removeLast();
+            // Pop small values from deque until condition isn't met, then add new value
+            while (!indexDeque.isEmpty() && nums[indexDeque.getLast()] < nums[rightPtr]) {
+                indexDeque.removeLast();
             }
-            idxQueue.addLast(rightPtr);
+            indexDeque.addLast(rightPtr); // Add index of larger element to deque
 
-            // Remove leftmost element from sliding window
-            if (leftPtr > idxQueue.getFirst()) {
-                idxQueue.removeFirst();
+            // If leftPtr is out of bounds, pop left-most element from deque
+            if (leftPtr > indexDeque.getFirst()) {
+                indexDeque.removeFirst();
             }
 
-            // Add elements to sliding window until its k-size
+            // If current sliding window is too big, get the left-most element from window and put it in result.
+            // Also, move leftPtr
             if (rightPtr + 1 >= k) {
-                result.add(nums[idxQueue.getFirst()]);
+                int maxNum = nums[indexDeque.getFirst()];
+                result.add(maxNum);
+
                 leftPtr++;
             }
+
             rightPtr++;
         }
 
-        // Transform List<Integer> to int[] and return it
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
