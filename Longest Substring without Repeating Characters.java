c72e69842517401612b7230ac5d56595343f85f1
Anthony <zamora.anthony1234@gmail.com>
2026-01-02 21:58:14 -0500

    Update Longest Substring without Repeating Characters.java

--- Longest Substring without Repeating Characters.java
+++ Longest Substring without Repeating Characters.java
@@ -1,8 +1,17 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// https://youtu.be/wiGpQwVHdE0?si=JAWf10zkRJdz1v2p
 /*
 Time Complexity: O(n) where n = length of input string
 
 Space Complexity: O(n) where n = length of set
+Thought Process
+- Create a set to keep track of elements in window
+- Create a left and right pointer
+- For every new and unique character in window, we add element to set and make the window bigger
+- If we find a non-unique character, we need to make the window smaller:
+    - First, we pop any elements from the left side to make the window smaller
+    - Second, if we pop the non-unique element, we continue iterating the string
+- Every time we move the right pointer, we calculate the substring length and update max length
 */
 
 class Solution {
@@ -11,25 +20,30 @@ class Solution {
             return s.length();
         }
 
-        // IMPORTANT: leftPtr and rightPtr will serve as the sliding window size
-        Set<Character> charSet = new HashSet<>();
-        int leftPtr = 0;
-        int maxLength = 0;
+        // Create a set
+        Set<Character> windowElems = new HashSet<>();
 
+        // Iterate string
+        int leftPtr = 0;
+        int maxLen = 0;
         for (int rightPtr = 0; rightPtr < s.length(); rightPtr++) {
-            Character curChar = Character.valueOf(s.charAt(rightPtr));
-            
-            // Bc we're using a sliding window, we need to pop elements from window and set until condition isn't met
-            while (charSet.contains(curChar)) {
-                Character farLeftChar = Character.valueOf(s.charAt(leftPtr));
-                charSet.remove(farLeftChar);
+            Character curLetter = s.charAt(rightPtr);
+
+            // If curLetter exist in window, pop the leftmost letter until curLetter doesn't exist
+            // This helps to make the window smaller until only unique elements exist in window
+            while (windowElems.contains(curLetter)) {
+                Character farLeftLetter = s.charAt(leftPtr);
+                windowElems.remove(farLeftLetter);
                 leftPtr++;
             }
 
-            charSet.add(curChar);
-            maxLength = Math.max(maxLength, (rightPtr - leftPtr) + 1); // "+1" because it starts counting at 0.
+            // Add new letter to window tracker and update maxLen
+            windowElems.add(curLetter);
+
+            // +1 because it starts counting at 0
+            maxLen = Math.max(maxLen, (rightPtr - leftPtr) + 1);
         }
 
-        return maxLength;
+        return maxLen;
     }
 }
