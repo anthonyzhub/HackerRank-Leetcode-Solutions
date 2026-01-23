8a0f753273ef213d0cced3ee86fe0b14701405d9
Anthony <zamora.anthony1234@gmail.com>
2026-01-22 19:24:50 -0500

    Update Koko Eating Bananas.java

--- Koko Eating Bananas.java
+++ Koko Eating Bananas.java
@@ -1,7 +1,12 @@
 // https://leetcode.com/problems/koko-eating-bananas/
 // https://youtu.be/U2SozAs9RzA?si=tgbHAiCeX131lXB_
 /*
-Time Complexity: O(log(n)) where n = maximum bananas in the array
+Time Complexity: O(log(m) * m) where m = maximum element inside given array. The goal is for Koko to eat all the piles of bananas.
+    It's assumed that there is a valid answer per case. Therefore, it's possible that the highest value is also the rate Koko must
+    follow to eat all the bananas. 
+
+    However, Koko wants to savor the bananas, so we need to find out how slow she can eat while eating all the bananas. That is why
+    we perform a binary search from 1 bananas to m bananas. We adjust the bananas-per-hour (bph) in every binary search iteration.
 
 Space Complexity: O(1)
 */
@@ -24,7 +29,6 @@ class Solution {
         // Pointers will be used to define k (bananas per hour)
         int leftPtr = 1;
         int rightPtr = getMaximumValue(piles);
-        System.out.println("rightPtr: " + rightPtr);
 
         // Create a return variable for later use
         // NOTE: We know for a fact that h >= len(piles) && Koko is able to eat an entire pile in 1 hour
